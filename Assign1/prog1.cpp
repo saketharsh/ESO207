05fd6291971700d22b5758830a14a2e63c8aab64
saketharsh <sharsh2010@gmail.com>
2017-08-17 12:45:38 +0530

    Assignment 1 Completed

--- Assign1/prog1.cpp
+++ Assign1/prog1.cpp
@@ -1,42 +1,59 @@
 #include <bits/stdc++.h>
 #include <complex>
+#include <cmath>
 using namespace std;
-typedef vector < complex < double > > imgarray
+typedef complex < float > complexnum; // Saving time using the typedef 
+typedef vector < complexnum >  dcomplex; // To construct a vector of complex numbers
+double  pi=  3.141592653589793*2;
 
 
-imgarray FFTfunc( imgarray ax, int num, bool work  ) {
-	if ( num ==0) 
-		return vector < int > ax;
-	imgarray evenarray;
-	imgarray oddarray;
+dcomplex FFTfunc( dcomplex ax, int num, bool work  ) {
+	if ( num ==1) 
+		return  ax;
+
+	dcomplex evenarray, oddarray, finalans(num);
 
 	for ( int i =0; i < num; i ++) {
 		if ( i%2==0)
 			evenarray.push_back(ax[i]);
 		else oddarray.push_back(ax[i]);
-	}
-	complex omega = work? inverse: normal;
-	// The actual logic will appear soon here.
-
-
+	}	
+  	complexnum iota = {0,1};
+  	float alpha = pi/num;
+  	alpha = work?-alpha:alpha;
+  	complexnum omega = exp(iota*alpha);
+  	complexnum theta= {1,0} ;
+  	dcomplex  Y_even= FFTfunc(evenarray, num/2, work);
+  	dcomplex Y_odd = FFTfunc(oddarray, num/2, work);
+  	for ( int i =0;i< (num/2); i++) {
+  		finalans[i]= Y_even[i] + theta*Y_odd[i];
+  		finalans[i + num/2 ]= Y_even[i] - theta*Y_odd[i];
+  		theta = theta*omega;
+  	}
+	return finalans;
 }
 
 int main() {
 	bool work;
 	int degree_bound;
-	imgarray polynomial;
 	cin>>work;
 	cin>>degree_bound;
-	for ( int i =0;i<degree_bound-1; i++) {
-		cin>>real>>imaginary;
-		complex < double >  coefficient = (real, imaginary);
-		polynomial.push_back(coefficient);
+	dcomplex polynomial;
+	float r, im;
+	for ( int i =0;i< degree_bound; i++) {
+		cin>>r>>im;
+		complexnum p = {r, im};
+		polynomial.push_back(p);
 	}
-	imgarray ans = FFTfunc( polynomial , degree_bound, work);
-	int len = ans.size();
-	for ( int i =0;i< len, i++) {
-		cout<<ans[i];
-	} 
+	dcomplex ansarray = FFTfunc(polynomial, degree_bound, work);
+	if ( work) {
+		for ( int i =0;i<ansarray.size(); i++) {
+			ansarray[i]= ansarray[i]/float(degree_bound);	
+		}
+	}
+	cout<<ansarray.size()<<" ";
+	for ( int i =0;i< ansarray.size(); i++) {
+		cout<<ansarray[i].real()<<" "<<ansarray[i].imag()<<" ";
+	}	 
 	return 0;
-
 }
